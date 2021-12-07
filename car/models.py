f06d0ff87c43b5da7dbb920fe991028664c81724
Ananya Ritu <ananyaritu8@gmail.com>
2021-12-07 21:04:43 +0600

    2nd commit

--- car/models.py
+++ car/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=80)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
